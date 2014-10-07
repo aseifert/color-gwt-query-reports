0f316377d75720b8db73ab05cfe70fabb429b3e8
Alexander Seifert <alexander.seifert@gmail.com>
2014-10-07 17:07:51 +0200

    add data to google chart object and let the library handle svg redrawing

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 Colors search query performance as shown by Neil Patel (but slightly altered) in his [blog post](http://www.quicksprout.com/2013/12/30/how-3-simple-google-analytics-reports-will-increase-your-search-engine-traffic/)
 
+Use in conjunction with a userscript manager like [Tampermonkey](http://tampermonkey.net/)
+
 
 Features:
 
 * colored CTR and Position fields
-* more accurate CTR on main query page
+* floating point accuracy for CTR on main query page
 
 
 TODO:
--- userstyle.js
+++ userstyle.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name         Color Google Webmaster Tools Search Query Performance
 // @namespace    http://www.alexauswien.at
+// @downloadURL  https://raw.githubusercontent.com/aseifert/color-gwt-query-reports/master/userstyle.js
 // @version      0.3
 // @description  Colors search query performance as shown by Neil Patel (but slightly altered) in his [blog post](http://www.quicksprout.com/2013/12/30/how-3-simple-google-analytics-reports-will-increase-your-search-engine-traffic/)
 // @match        https://www.google.com/webmasters/tools/top-search-queries*
@@ -39,36 +40,39 @@
     var BAD_POS_CTR_CUTOFF  = 3
 
 
-    // color stuff
+    // go through the rows do our magic
     var rows = document.querySelectorAll("table#grid > tbody > tr")
-
     for (var i=0; i < rows.length; i++) {
         var row = rows[i]
 
+        // get impressions
         var imps = row.getElementsByClassName("impressions")[0]
         var impsValue = parseInt(imps.textContent.replace(/[,\.]/, ""))
         if (impsValue < MIN_IMPRESSIONS) continue
 
+        // get clicks
         var clicks = row.getElementsByClassName("clicks")[0]
         var clicksValue = parseInt(clicks.textContent.replace(/[,\.]/, ""))
 
+        // color position
         var pos = row.getElementsByClassName("position")[0]
         processPosition(pos)
 
+        // make CTR more exact
         var ctr = row.getElementsByClassName("click-rate")[0]
         // set element to a more exact CTR value
         ctr_exact = 100.0 * (clicksValue / impsValue)
         ctr.innerHTML = parseInt( (ctr_exact+0.005) *100) / 100.0 + "&nbsp;%"
 
+        // color CTR
         processCTR(ctr, pos)
     }
 
-    // wait 3 seconds for the svg to load and then add a CTR graph to the svg
-    var svg;
-    setTimeout(function() {
-        svg = document.querySelector("svg")
-        processSVG(svg)
-    }, 3000);
+    // add data for the CTRs
+    var chartData = getChartData()
+    chartData = augmentChartData(chartData)
+    var chart = createChart(chartData)
+    chart.render()
 
     function processPosition(pos) {
         var posValue = parseFloat(pos.textContent.replace(",", "."))
@@ -100,84 +104,66 @@
         }
     }
 
-    function processSVG(svg) {
-        var graph       = svg.querySelector("g").querySelector("g").lastChild
-        var impressions = graph.firstChild
-        var clicks      = graph.lastChild
+    function getChartData() {
+        var script     = document.querySelector("#tsq-chart").parentNode.nextElementSibling
+        if (script.tagName !== "SCRIPT") throw new Error("Chart data not found")
+        var dataString = script.textContent.match(/setData\((.+)\);/)[1]
+        return JSON.parse( dataString )
+    }
 
-        var impressionsPath     = impressions.getAttribute("d")
-        var impressionsCommands = impressionsPath.split(/[LM]/)
-        var clicksPath          = clicks.getAttribute("d")
-        var clicksCommands      = clicksPath.split(/[LM]/)
+    // sprad out values vertically accross the svg
+    function calculateSpread(ctrs, clicks, impressions) {
+        var max  = Math.max.apply(null, ctrs)
+        
+        var maxC = Math.max.apply(null, clicks)
+        var maxI = Math.max.apply(null, impressions)
+        var maxOther = Math.max(maxC, maxI)
 
-        var SVG_HEIGHT   = 140
-        var MAGIC_NUMBER = 21
+        return maxOther / max
+    }
 
-        // sadly I don't know what I did here
-        function estimateGoogleY(y) {
-            return SVG_HEIGHT - MAGIC_NUMBER - y / 1.2
-        }
+    // changes the data array
+    function augmentChartData(data) {
 
-        // y as offset from the top
-        function calculateCtrY(y) {
-            return SVG_HEIGHT - y
-        }
+        // add new column for CTRs
+        data.cols.push({
+            id: "Column_2",
+            label: "CTR",
+            pattern: "",
+            type: "number"
+        })
 
-        // sprad out values vertically accross the svg
-        function calculateSpread(values) {
-            var max = Math.max.apply(null, values)
-            return (SVG_HEIGHT - MAGIC_NUMBER) / max
-        }
+        // calculate CTRs and gather spreads and impressions
+        ctrs        = []
+        clicks      = []
+        impressions = []
+        data.rows.forEach(function(row, i) {
+            impressions.push( row.c[1].v )
+            clicks.push( row.c[2].v )
+            ctrs.push( 100.0 * clicks[i] / impressions[i] )
 
-        var ctrs = []
-        // go through all the impressions and clicks and calculate the ratio
-        for (var i=1; i < clicksCommands.length; i++) {
-            // a command looks like this: "<cmd_name><x_coord>,<y_coord>"
-            var clicksCommand      = clicksCommands[i]
-            var impressionsCommand = impressionsCommands[i]
-            var x = parseFloat(clicksCommand.split(",")[0] || 0)
+        })
 
-            var clicksY      = estimateGoogleY(parseFloat(clicksCommand.split(",")[1]))
-            var impressionsY = estimateGoogleY(parseFloat(impressionsCommand.split(",")[1]))
+        // add values adjusted by spread to the data
+        var spread = calculateSpread(ctrs, clicks, impressions)
+        data.rows.forEach(function(row, i) {
+            row.c.push({
+                v: spread * ctrs[i],
+                f: ""+ctrs[i]
+            })
+        })
 
-            var ctr = clicksY / impressionsY
-            ctrs.push(ctr)
-        }
+        return data
+    }
 
-        // y coordinates should be spread out across the svg
-        var spread = calculateSpread(ctrs)
-        var coords = []
-        for (var i=1; i < clicksCommands.length; i++) {
-            // we'll just take the x coord from the clicks commands
-            var clicksCommand = clicksCommands[i]
-            var x = parseFloat(clicksCommand.split(",")[0] || 0)
+    function createChart(data) {
+        var chart = new wmt.common.display.charts.Chart(
+        "LineChart",
+        "tsq-chart",
+        {"chartArea":{"height":"140","width":"100%","left":"0","top":"0"},"annotation":{"1":{"style":"line"}},"height":"160","pointSize":0,"colors":["#4d90fe","#dd4b39","#3d9400","#cc9933","#994499","#fb9d00","#c9678e","#2ad0b1","#954e04"],"legend":{"position":"none"},"width":"100%","vAxis":{"baselineColor":"#848484","textPosition":"in","textStyle":{"bold":false,"color":"#848484","fontSize":9},"baseline":0},"focusTarget":"category","hAxis":{"baselineColor":"#848484","textPosition":"out","textStyle":{"bold":false,"color":"#848484","fontSize":9},"showTextEvery":4}});
 
-            var y = ctrs[i-1]
-            y = calculateCtrY( y * spread )
-            coords.push( [x, parseInt(y)] )
-        }
-
-        // create SVG path from the coorinates
-        var pathString = "M" + coords[0][0] + "," + coords[0][1]
-        for (var i=1; i < coords.length; i++) {
-            pathString += "L" + coords[i][0] + "," + coords[i][1]
-        }
-
-        // create new HTML element and append to the svg graph
-        var newPath = document.createElementNS("http://www.w3.org/2000/svg", 'path')
-        newPath.style.stroke      = "lightgreen"
-        newPath.style.strokeWidth = "2"
-        newPath.style.fillOpacity = "1"
-        newPath.style.fill        = "none"
-        newPath.setAttribute("d", pathString)
-        graph.appendChild(newPath)
-
-        // TODO: y labels on the right side
-
-        // <text text-anchor="start" x="1200" y="11.15" font-family="Arial" font-size="9" stroke="none" stroke-width="0" fill="#848484">120</text>
-        // <text text-anchor="start" x="1200" y="45.89" font-family="Arial" font-size="9" stroke="none" stroke-width="0" fill="#848484">90</text>
-        //<text text-anchor="start" x="1200" y="80.65" font-family="Arial" font-size="9" stroke="none" stroke-width="0" fill="#848484">60</text>
-        //<text text-anchor="start" x="1200" y="115.4" font-family="Arial" font-size="9" stroke="none" stroke-width="0" fill="#848484">30</text>
+        chart.setData(data)
+        return chart
     }
 
 })()
